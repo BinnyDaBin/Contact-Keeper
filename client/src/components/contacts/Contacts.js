50e811bbd803af71bc58631ab2417545fc49a51c
Dabin Lee <dabinlee0918@gmail.com>
2019-07-10 15:48:47 -0400

    feat($contacts): implement contacts UI

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,14 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import ContactItem from './ContactItem';
+import ContactContext from '../../context/contact/contactContext';
 
-const Contacts = ({ getContacts, contacts }) => {
-  useEffect(() => {
-    getContacts();
-    // eslint-disable-next-line
-  }, []);
+const Contacts = () => {
+  const contactContext = useContext(ContactContext);
 
-  // const { msg } = contacts;
+  const { contacts, filtered } = contactContext;
 
-  return <div>{contacts}</div>;
+  if (contacts.length === 0) {
+    return <h4>Please add a contact</h4>;
+  }
+
+  return (
+    <Fragment>
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map(contact => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))
+          : contacts.map(contact => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem key={contact.id} contact={contact} />
+              </CSSTransition>
+            ))}
+      </TransitionGroup>
+    </Fragment>
+  );
 };
 
 export default Contacts;
